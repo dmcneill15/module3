4b7e6260704c25497edd4c8addc7c847afd85982
dmcneill15 <dvveijeren@gmail.com>
2024-06-12 20:24:54 +1200

    Question 10

--- JSIntermediate_LabExercises.js
+++ JSIntermediate_LabExercises.js
@@ -1,6 +1,6 @@
 //Question 1
 //-------------------------------------------------------------------------------
-/*
+
 function ucFirstLetters(str){
     let splitStr = str.split(' ');
     for (let i = 0; i < splitStr.length; i++) {
@@ -9,13 +9,13 @@ function ucFirstLetters(str){
     return splitStr.join(' ');
 }
 
-console.log(ucFirstLetters("los angeles") ) //Los Angeles
-console.log(ucFirstLetters("hello world, it's me!") ) //Los Angeles
-*/
+//console.log(ucFirstLetters("los angeles") ) //Los Angeles
+//console.log(ucFirstLetters("hello world, it's me!") ) //Los Angeles
+
 
 //Question 2
 //-------------------------------------------------------------------------------
-/*
+
 function truncate(str, max){
     if(str.length < max){
         return str;
@@ -28,12 +28,12 @@ function truncate2(str, max){
     return((str.length < max) ? str : (str.slice(0, max) + "...")); 
 }
 
-console.log(truncate2('This text will be truncated if it is too long', 25)) // This text will be truncat...
-*/
+//console.log(truncate2('This text will be truncated if it is too long', 25)) // This text will be truncat...
+
 
 //Question 3
 //-------------------------------------------------------------------------------
-/*
+
 function replaceMiddleAnimal(arr, newValue){
     let midIndex = arr.length / 2;
     arr[midIndex] = newValue;
@@ -60,13 +60,13 @@ replaceMiddleAnimal(animals, "Leopard");
 
 let newAnimals = findMatchingAnimals(animals, "ZEb");
 
-console.log(animals);
-console.log(newAnimals);
-*/
+//console.log(animals);
+//console.log(newAnimals);
+
 
 //Question 4
 //-------------------------------------------------------------------------------
-/*
+
 function camelCase(cssProp) {
   let dashIndex = cssProp.indexOf("-");
   if (dashIndex < 0) {
@@ -102,18 +102,18 @@ function camelCase3(cssProp) {
     return cssPropNew;
   }
 
-console.log(camelCase("margin-left")); // marginLeft
-console.log(camelCase("background-image")); // backgroundImage
-console.log(camelCase("display")); // display
-console.log("camelCase2: " + camelCase2("background-image")); // backgroundImage
-console.log("camelCase2: " + camelCase2("display")); // display
-console.log("camelCase3: " + camelCase3("background-image")); // backgroundImage
-console.log("camelCase3: " + camelCase3("display")); // display
-*/
+//console.log(camelCase("margin-left")); // marginLeft
+//console.log(camelCase("background-image")); // backgroundImage
+//console.log(camelCase("display")); // display
+//console.log("camelCase2: " + camelCase2("background-image")); // backgroundImage
+//console.log("camelCase2: " + camelCase2("display")); // display
+//console.log("camelCase3: " + camelCase3("background-image")); // backgroundImage
+//console.log("camelCase3: " + camelCase3("display")); // display
+
 
 // Question 5
 //-------------------------------------------------------------------------------
-/*
+
 function currencyAddition(float1, float2) {
   return parseFloat((float1 + float2).toFixed(2));
   //floats are added correctly
@@ -144,15 +144,15 @@ let twentyCents = 0.22;
 let tenCents = 0.11;
 let fixedTwenty = twentyCents.toFixed(2);
 let fixedTen = tenCents.toFixed(2);
-console.log(fixedTwenty + fixedTen); //why is this not working? - output is concatenating the two variables as strings
-console.log(currencyAddition(twentyCents, tenCents));
-console.log(currencyOperation(twentyCents, tenCents, "add", 2));
-console.log(currencyOperation(twentyCents, tenCents, "divide", 4));
-*/
+//console.log(fixedTwenty + fixedTen); //why is this not working? - output is concatenating the two variables as strings
+//console.log(currencyAddition(twentyCents, tenCents));
+//console.log(currencyOperation(twentyCents, tenCents, "add", 2));
+//console.log(currencyOperation(twentyCents, tenCents, "divide", 4));
+
 
 // Question 6
 //-------------------------------------------------------------------------------
-/*
+
 function unique(duplicatesArray){
     let newArray = new Set(duplicatesArray);  //Set function creates a Set from the original array, which automatically removes any duplicate values
     return [...newArray];   //convert the set into an array
@@ -161,15 +161,15 @@ function unique(duplicatesArray){
 const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'red', 'blue', 'yellow'];
 const testScores = [55, 84, 97, 63, 55, 32, 84, 91, 55, 43];
 const testDuplicates = ['Tiger', 'Tiger', 'Giraffe', 'Giraffe', 'Elephant', 'Elephant', 'Lion', 'Lion'];
-console.log(unique(colours)); // [ 'red', 'green', 'blue', 'yellow', 'orange' ] 
-console.log(unique(testScores)); // [ 55, 84, 97, 63, 32, 91, 43 ]
-console.log(unique(testDuplicates));
-*/
+//console.log(unique(colours)); // [ 'red', 'green', 'blue', 'yellow', 'orange' ] 
+//console.log(unique(testScores)); // [ 55, 84, 97, 63, 32, 91, 43 ]
+//console.log(unique(testDuplicates));
+
 
 //Question 7
 //-------------------------------------------------------------------------------
 //https://sentry.io/answers/find-object-by-property-in-javascript-array/#:~:text=The%20find%20method%20iterates%20through,iterating%20and%20return%20that%20element.
-/*
+
 function getBookTitle(bookList, bookId){
     return bookList.find(obj => obj.id == bookId).title;
 }
@@ -212,11 +212,12 @@ const books = [
 //console.log(getBookTitle(books, 1));
 //console.log(getOldBooks(books));
 //console.log(addGenre(books));
-console.log(getTitles(books, "H"));
-*/
+//console.log(getTitles(books, "H"));
+
 
 //Question 8
 //-------------------------------------------------------------------------------
+
 function printPhoneBook(contacts){
   contacts.forEach((value, key) => {
     console.log("Name: " + key + ", Number: " + value);
@@ -245,5 +246,86 @@ phoneBookDEF.forEach((value, key) => {
   phoneBook.set(key, value);
 });
 
-console.log(phoneBookABC);
-printPhoneBook(phoneBook);
+//console.log(phoneBookABC);
+//printPhoneBook(phoneBook);
+
+
+//Question 9
+//-------------------------------------------------------------------------------
+function sumSalaries(salaries){
+  let sum = 0;
+  
+  for(let key in salaries){
+    sum = sum + salaries[key];
+  }
+  return sum;
+}
+
+function topEarner(salaries){
+  let greatestSalary = salaries[Object.keys(salaries)[0]];  //by default assign the first key as the largest
+  let nameOfGreatestSalary = null;
+
+ //  salaries.forEach(item => {
+ //   Object.keys(item).forEach(key=>{
+ //     if(item[key] > greatestSalary){
+ //       greatestSalary = item[key];
+ //       nameOfGreatestSalary = key;
+ //     }
+ //   })
+ // }) //note - can't use this method because the given json object is not an array
+
+for (let key in salaries){
+  if(salaries[key] > greatestSalary){
+    greatestSalary = salaries[key];
+    nameOfGreatestSalary = key;
+  }
+}
+
+  return nameOfGreatestSalary;
+}
+
+let salaries = { 
+  "Timothy" : 35000, 
+  "David" : 25000, 
+  "Mary" : 55000, 
+  "Christina" : 75000, 
+  "James" : 43000 
+};
+
+//console.log(sumSalaries(salaries));
+//console.log(topEarner(salaries));
+
+//Question 10
+//-------------------------------------------------------------------------------
+function daysInBetween(date1, date2){
+    const diffInTime = date2.getTime() - date1.getTime(); //time diff in milliseconds
+    const milliInOneDay = 24*60*60*1000; //24hrs*60min*60sec*milli
+    const diffInDays = Math.round(diffInTime/ milliInOneDay);
+
+    return diffInDays;
+}
+
+
+const today = new Date(); 
+console.log('Current time is ' + today.toLocaleTimeString()); 
+console.log(today.getHours() + ' hours have passed so far today');
+console.log((today.getMinutes()+today.getHours()*60) + ' minutes have passed so far today');
+console.log((today.getSeconds()+today.getMinutes()*60+today.getHours()*60*60) + ' seconds have passed so far today');
+
+const myBday = new Date(1995, 10-1, 30); //month is zero indexed
+const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let ageYear = today.getFullYear() - myBday.getFullYear();
+let ageMonth = today.getMonth() - myBday.getMonth();
+let ageDays = today.getDate() - myBday.getDate();
+if(ageDays <= 0){
+  ageDays = ageDays + monthDays[myBday.getMonth()];
+}
+if (ageMonth < 0){  //if the current month is before birth month, subtract a year
+  ageYear--;
+  ageMonth += 12;
+}
+console.log('I am ' + ageYear + ' years , ' + ageMonth + ' months and ' + ageDays + ' days old');
+
+let date1 = new Date("06/12/2024");
+let date2 = new Date("10/30/2024");
+console.log("difference in days:", daysInBetween(date1, date2));
